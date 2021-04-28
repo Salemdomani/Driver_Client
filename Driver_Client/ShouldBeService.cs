75770ca83e597d4b0aa11bcf45dbedbf05687fe2
asus <asus@DESKTOP-P4F8D8S>
2021-04-28 22:34:15 +0300

    Add files via upload

--- Driver_Client/ShouldBeService.cs
+++ Driver_Client/ShouldBeService.cs
@@ -27,7 +27,7 @@ namespace Driver_Client
         public ShouldBeService()
         {
             InitializeComponent();
-            VmNum = int.Parse(Environment.UserName.Where(char.IsDigit).ToArray().ToString());
+            VmNum = int.Parse(new string(Environment.UserName.Where(char.IsDigit).ToArray()));
             client = new Client(host, 12000);
             server = new TcpServer(host, 11000, this);
             chrome = new Chrome();
